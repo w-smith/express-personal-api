93c63e0dcc9c09ac917a13fa66eabdf15c8b85fa
w-smith <wsmith@bex.net>
2018-01-03 16:55:24 -0700

    starting server

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || 
                   process.env.MONGOLAB_URI || 
                   process.env.MONGOHQ_URL || 
-                  "mongodb://localhost/personal-api");
+                  "mongodb://localhost/3000");
 
 // module.exports.Campsite = require("./campsite.js.example");
