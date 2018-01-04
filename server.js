57523a800ec3c0aa0af18159b1303e476128acde
w-smith <wsmith@bex.net>
2018-01-03 17:27:30 -0700

    changing endpoints

--- server.js
+++ server.js
@@ -38,14 +38,14 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    // woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
+    message: "Welcome to Greg's place.",
+    documentation_url: "https://github.com/w-smith/express_self_api/README.md", 
+    base_url: "http://still-shore-68963.herokuapp.com", 
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api", description: "Mah endpoints"},
+      {method: "GET", path: "/api/profile", description: "DIS ABOUT ME"},
+      {method: "POST", path: "/api/videogames", description: "Some Video Games I like"}
     ]
   })
 });
